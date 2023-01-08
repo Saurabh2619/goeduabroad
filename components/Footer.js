3a4bb4dfebba910eea0915ede7c2396eeff1be40
Naman Sharma <officialnmn@gmail.com>
2023-01-08 23:00:47 +0530

    main

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,107 @@ import styles from './Footer.module.css';
 
 function Footer(){
 
-    return <footer className={styles.footer}>Footer</footer>
+
+    const socials = [
+{
+    link:'#',
+    title:'Facebook',
+},
+{
+    link:'#',
+    title:'LinkedIn',
+},
+{
+    link:'#',
+    title:'Instagram',
+},
+{
+    link:'#',
+    title:'WhatsApp',
+},
+{
+    link:'#',
+    title:'YouTube',
+},
+    ]
+    const quicks = [
+        {
+            link:'#',
+            title:'Home',
+        },
+        {
+            link:'#',
+            title:'Countries',
+        },
+        {
+            link:'#',
+            title:'About',
+        },
+        {
+            link:'#',
+            title:'Careers',
+        },
+        {
+            link:'#',
+            title:'Contact',
+        },
+            ]
+
+            const resources = [
+                {
+                    link:'#',
+                    title:'Privacy Policy',
+                },
+                {
+                    link:'#',
+                    title:'Terms & Conditions',
+                },
+                {
+                    link:'#',
+                    title:'Refund Policy',
+                },
+                {
+                    link:'#',
+                    title:'Sitemap',
+                },
+               
+                    ]
+    return <footer className={styles.footerouter}><div className={styles.footer}>
+        <div className={styles.top}>
+            <div className={styles.badge}><img src="/label.svg"/></div>
+            <img className={styles.footerlogo} src='/edulogo.svg'/>
+            <div className={styles.cols}>
+<div className={styles.col}>
+<h2>Quick Links</h2>
+{quicks && quicks.map((i,d)=>{
+    return <a className={styles.link} href={i.link}><li>{i.title}</li></a>
+})}
+</div>
+<div className={styles.col}>
+<h2>Useful Resources</h2>
+{resources && resources.map((i,d)=>{
+    return <a className={styles.link} href={i.link}><li>{i.title}</li></a>
+})}
+</div>
+<div className={styles.col}>
+<h2>Social Links</h2>
+{socials && socials.map((i,d)=>{
+    return <a className={styles.link} href={i.link}><li>{i.title}</li></a>
+})}
+</div>
+
+            </div>
+            <div className={styles.third}>
+            <button className={styles.expert}>Talk to an Expert</button></div>
+            <div className={styles.inner}>
+                <span><a href="tel:+919999999999">Contact : +91 999 999 9999</a> | <a href="mailto:info@goeduabroad.com">Email : info@goeduabroad.com</a></span>
+            </div>
+        </div>
+        <div className={styles.bottom}>
+            <p>2023© All Rights Reserved | Edu Abroad</p>
+            <a href="https://blog.nmnvisuals.com">Developed & Designed by NMN Visuals</a>
+        </div>
+        </div></footer>
 }
 
 export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,88 @@ import styles from './Home.module.css'
 import DefaultLayout from '../layouts/DefaultLayout'
 import Section from '../components/Section'
 
-
+import Marquee from "react-fast-marquee";
 
 export default function Home() {
 
+const herocountries = [
+  {
+    title:'Antarctica',
+    image:'/ant.png',
+    color:'skyblue',
+  },
+  {
+    title:'Australia',
+    image:'/aus.png',
+    color:'skyblue',
+  },
+  {
+    title:'United Kingdom',
+    image:'/uk.png',
+    color:'skyblue',
+  },
+  {
+    title:'Japan',
+    image:'/jp.png',
+    color:'skyblue',
+  },
+  {
+    title:'London',
+    image:'/uk.png',
+    color:'skyblue',
+  },
+  {
+    title:'United States',
+    image:'/us.png',
+    color:'skyblue',
+  },
 
+]
+
+const partners=[
+  {
+    image:'/1.png',
+    title:'XYZ University',
+    image2:'/2.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/3.png',
+    title:'XYZ University',
+    image2:'/4.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/5.png',
+    title:'XYZ University',
+    image2:'/6.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/7.png',
+    title:'XYZ University',
+    image2:'/8.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/9.png',
+    title:'XYZ University',
+    image2:'/10.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/11.png',
+    title:'XYZ University',
+    image2:'/12.png',
+    title2:'XYZ University',
+  },
+  {
+    image:'/13.png',
+    title:'XYZ University',
+    image2:'/14.png',
+    title2:'XYZ University',
+  }
+]
 const countries = [
   
   {
@@ -38,32 +115,47 @@ image:'/brazil.jpg'
       }
 
 ]
+const authorities =[
 
+  {
+    image:'https://www.nasa.gov/sites/default/files/thumbnails/image/nasa-logo-web-rgb.png',
+    title:'NASA'
+  },
+  {
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Government_of_India_logo.svg/1200px-Government_of_India_logo.svg.png',
+    title:'Indian Government'
+  },
+  {
+    image:'https://www.un.org/youthenvoy/wp-content/uploads/2014/09/unesco-logo-260px.jpg',
+    title:'UNESCO'
+  },
+
+]
 
 const universities = [
   
   {
 title:'Cambridge University',
 description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-stars:4,
+
 image:'/camb.jpg'
 },
 {
   title:'Harvard University',
   description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-  stars:4,
+  
   image:'/harvard.jpg'
   },
   {
     title:'John Hopkins',
     description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-    stars:4,
+    
     image:'/hopkins.jpg'
     },
     {
       title:'MIT',
       description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-      stars:4,
+      
       image:'/mit.jpg'
       }
 
@@ -79,7 +171,24 @@ image:'/camb.jpg'
         
       </Head>
       <DefaultLayout>
+        <div className={styles.hero}>
+<div className={styles.mask}></div>
+<div
+className={styles.content}>
+  <h2><span className='b1'>An Initiative to</span><br/>Build Career Abroad</h2>
+  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+<div className={styles.getstarted}><input className={styles.input} placeholder="Enter your email address"></input><button>Get Started</button></div>
+<div className={styles.top}>
+  <h3>Top Countries to Study Abroad :</h3>
+  <div className={styles.conts}>
+{herocountries && herocountries.map((i,d)=>{
 
+  return <div className={styles.cont}><img src={i.image}/> <p>{i.title}</p></div>
+})}</div>
+</div>
+</div>
+        </div>
+      <Section title="About Us" align="left" color="var(--brand-col1)"></Section>
 <Section title="Top Cities of 2023 to : Study Abroad" align="center" color="var(--brand-col1)">
 
 <div className={styles.cardhold}>
@@ -102,6 +211,16 @@ return <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.85 1
 
 </div>
 </Section>
+<div className={styles.students}>
+<div className={styles.left}>
+<h2>Join 9000+ students <br/>who joined <span className='b1'>Edu Abroad</span></h2>
+<button onClick={()=>{}}>Join Now</button>
+</div>
+<div className={styles.right}>
+  <div className={styles.circle}><img src='/gras.png'/></div>
+</div>
+</div>
+<Section title="Why Choose :Edu Abroad?" align="left" color="var(--brand-col1)"></Section>
 <Section title="Top Universities of 2023" align="center" color="var(--brand-col1)">
 
 <div className={styles.cardhold}>
@@ -113,7 +232,7 @@ return <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.85 1
       <h2>{i.title}</h2>
       <p>{i.description.substring(0,260)}...</p>
       <div className={styles.stars}>
-      {Array(i.stars).fill().map((i,d)=>{
+      {Array(i.stars ? i.stars : 0).fill().map((i,d)=>{
 return <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.85 17.92" fill="#f7ae1e"><defs></defs><g id="Layer_1-2"><polygon class="cls-1" points="9.42 0 12.33 5.9 18.85 6.85 14.13 11.44 15.25 17.92 9.42 14.86 3.6 17.92 4.71 11.44 0 6.85 6.51 5.9 9.42 0"/></g></svg>
 
     })}</div></div>
@@ -124,6 +243,40 @@ return <svg id="Layer_2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.85 1
 
 </div>
 </Section>
+<Section title="Partner Universities" align="center" color="var(--brand-col1)">
+<Marquee speed={80}>
+  {partners && partners.map((i,d)=>{
+
+    return <div className={styles.partners}>
+      <div className={styles.university}><img src={i.image}/><p>{i.title}</p></div>
+      <div className={styles.university}><img src={i.image2}/><p>{i.title2}</p></div>
+    </div>
+  })}
+</Marquee>
+</Section>
+
+<Section title="Mentors" align="center" color="var(--brand-col1)"></Section>
+<div className={styles.authorities}>
+<h2 className={styles.heading}>Trusted by Giant Authorities</h2>
+<div className={styles.marqueecont}>
+<Marquee speed={80}>
+  {authorities && authorities.map((i,d)=>{
+
+    return <div className={styles.partners}>
+      <div className={styles.university}><img src={i.image}/><p>{i.title}</p></div>
+      
+    </div>
+  })}
+  {authorities && authorities.map((i,d)=>{
+
+return <div className={styles.partners}>
+  <div className={styles.university}><img src={i.image}/><p>{i.title}</p></div>
+  
+</div>
+})}
+</Marquee>
+</div>
+</div>
       </DefaultLayout>
     </>
   )
