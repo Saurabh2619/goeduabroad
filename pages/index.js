b914c6f6840e8f3a6fb228b4544c43eab9e093b7
Naman Sharma <officialnmn@gmail.com>
2023-03-01 13:56:13 +0530

    fea

--- pages/index.js
+++ pages/index.js
@@ -418,13 +418,13 @@ const courses = [
 title:'Academic Training',
 description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
 
-image:'https://cdn.dribbble.com/users/3956332/screenshots/15328827/online_courses_4x.jpg'
+image:'https://blog.smartabroad.in/wp-content/uploads/2022/08/studying-student-on-desk.jpg'
 },
 {
   title:'General Training',
   description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
   
-  image:'https://img.freepik.com/premium-vector/online-courses-concept_23-2148524391.jpg'
+  image:'https://blog.advancementcourses.com/wp-content/uploads/2019/07/800x450-firsttimeteachers-header-ac.jpg'
   },
  
 
