69e0d1605decc924df8b6db5c96b4e76b666b620
Naman Sharma <officialnmn@gmail.com>
2023-04-13 15:56:56 +0530

    update

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,4 +1,4 @@
-import { getServerSideSitemap } from "next-sitemap";
+import { getServerSideSitemap, getServerSideSitemapLegacy } from "next-sitemap";
 import { supabase } from "../../utils/supabaseClient";
 
 export const getServerSideProps = async(ctx) => {
@@ -12,32 +12,32 @@ export const getServerSideProps = async(ctx) => {
     if (data && datgr && datapr) {
 
         r = data.data.map((i, d) => ({
-            loc: `https://goeduabroad.com/services/${i.slug}`,
-            lastmod: `${i.created_at.substring(0,10)}`,
+            "loc": `https://goeduabroad.com/services/${i.slug}`,
+            "lastmod": `${i.created_at.substring(0,10)}`,
         }))
         const ne = datgr.data.map((i, d) => ({
-            loc: `https://goeduabroad.com/post/${i.slug}`,
-            lastmod: `${i.created_at.substring(0,10)}`,
+            "loc": `https://goeduabroad.com/post/${i.slug}`,
+            "lastmod": `${i.created_at.substring(0,10)}`,
         }))
         const net = datapr.data.map((i, d) => ({
-            loc: `https://goeduabroad.com/studyabroad/${i.slug}`,
-            lastmod: `${i.created_at.substring(0,10)}`,
+            "loc": `https://goeduabroad.com/studyabroad/${i.slug}`,
+            "lastmod": `${i.created_at.substring(0,10)}`,
         }))
         const neto = datapro.data.map((i, d) => ({
-            loc: `https://goeduabroad.com/testpreps/${i.slug}`,
-            lastmod: `${i.created_at.substring(0,10)}`,
+            "loc": `https://goeduabroad.com/testpreps/${i.slug}`,
+            "lastmod": `${i.created_at.substring(0,10)}`,
         }))
         const stat = [{
-            loc: `https://goeduabroad.com/contact`,
+            "loc": `https://goeduabroad.com/contact`,
 
         }]
         r = [...r, ...ne, ...stat,...net,...neto];
-        console.log(r)
+     console.table(r)
 
     }
 
     const field = [...r];
-    return getServerSideSitemap(ctx,field)
+    return getServerSideSitemapLegacy(ctx,field)
 }
 
 export default function Site() {}
