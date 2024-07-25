cd4f5b1c8a8db1040efffd499dee1f9725aa891d
Naman <naman@nmnvisuals.com>
2024-07-25 17:51:57 +0530

    test

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -158,10 +158,10 @@ if(data){
         <NextSeo
       title={final.title }
       description={`${final.metaDesc}| Best Study Abroad Consultant in India`}
-      canonical={`https://goeduabroad.com/services/${final?.slug}`}
+      canonical={`https://goeduabroad.com/post/${final?.slug}`}
       openGraph={{
         type: "article",
-        url: `https://goeduabroad.com/services/${final?.slug}`,
+        url: `https://goeduabroad.com/post/${final?.slug}`,
         title: final?.title + " | EduAbroad",
         description : final && final.metaDesc? final.metaDesc : final?.title,
         article: {
