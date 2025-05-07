2052d5d4b451fae28ba68ca3d77f8281185bb288
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-05-07 11:53:09 +0530

    again

--- pages/youngpreneurs.js
+++ pages/youngpreneurs.js
@@ -831,7 +831,7 @@ export default function Youngpreneurs() {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
-              fill="none"
+              fill="none" 
               viewBox="0 0 24 24"
               stroke="currentColor"
             >
