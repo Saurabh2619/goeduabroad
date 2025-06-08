9ed95d948bf710e5c7903b1d16d1c7955aee53e3
Saurabh2619 <sharmasaurabh2606@gmail.com>
2025-06-08 11:51:23 +0530

    meta pixel code

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ export default function Document() {
             `
           }}
         />
-        
+
         {/* Google Ads Tag */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17023337864"></script>
         <script
@@ -29,9 +29,36 @@ export default function Document() {
             `
           }}
         />
-      </Head>
 
+        {/* Meta Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1228322315338327');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+      </Head>
       <body>
+        {/* Meta Pixel NoScript Fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1228322315338327&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
