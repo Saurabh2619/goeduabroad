59912cfdfaa5e857ad2e059f0bde7b0534f094aa
Saurabh2619 <sharmasaurabh2606@gmail.com>
2026-07-24 16:54:19 +0530

    code gtm

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),
+                  dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;
+              j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NS86VPDJ');
+          `}
+        </Script>
+
         {/* Google Analytics Tag */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-JCN6PHHP2R"></script>
         <script
@@ -49,6 +66,16 @@ export default function Document() {
         />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NS86VPDJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {/* Meta Pixel NoScript Fallback */}
         <noscript>
           <img
